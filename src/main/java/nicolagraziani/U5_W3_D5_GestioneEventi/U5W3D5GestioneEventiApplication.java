be053ab411fb5dfa8e6cf472b4883e9d4e9c03ca
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-04-24 10:52:26 +0200

    Initial setup (Spring Boot, dependencies, DB config)

---
+++ src/main/java/nicolagraziani/U5_W3_D5_GestioneEventi/U5W3D5GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package nicolagraziani.U5_W3_D5_GestioneEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nicolagraziani/U5_W3_D5_GestioneEventi/U5W3D5GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package nicolagraziani.U5_W3_D5_GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
